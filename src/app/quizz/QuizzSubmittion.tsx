968294928e1987fd28728d365a51e4eefae55da2
Lloyd Boone <lboone@progressive.com>
2024-05-09 18:03:05 -0400

    implement quizz interface and workflow

--- src/app/quizz/QuizzSubmittion.tsx
+++ src/app/quizz/QuizzSubmittion.tsx
@@ -1,30 +1,49 @@
+import { useEffect} from "react";
 import Bar from "@/components/Bar";
+import Image from "next/image";
+import { balloons, useReward } from "react-rewards";
 
 type Props = {
-    scorePercentage: number,
-    score: number,
-    totalQuestions: number,
-}
+  scorePercentage: number;
+  score: number;
+  totalQuestions: number;
+};
 const QuizzSubmittion = (props: Props) => {
-    const { scorePercentage, score, totalQuestions } = props;
-
+  const { scorePercentage, score, totalQuestions } = props;
+  const { reward } = useReward('rewardId',"confetti" );
+    useEffect(() => {
+        if(scorePercentage === 100){
+            reward();
+        }
+    }, [scorePercentage, reward]);
   return (
     <div className="flex flex-col flex-1">
-        <main className="py-11 flex flex-col gap-4 items-center flex-1 mt-24">
-            <h2 className="text-3xl font-bold">Quizz Complete!</h2>
-            <p>You scored: {scorePercentage}%</p>
-            <>
+      <main className="py-11 flex flex-col gap-4 items-center flex-1 mt-24">
+        <h2 className="text-3xl font-bold">Quizz Complete!</h2>
+        <p>You scored: {scorePercentage}%</p>
+        { scorePercentage === 100 ? 
+            <div className="flex flex-col items-center">
+                <p className="text-2xl text-primary uppercase">🎉 Congratulations! 🎉</p>
+                <div className="flex justify-center">
+                    <Image  src="/images/owl-smiling.png" alt="Owl Smiling" width={400} height={400} />
+                </div>
+                <span id="rewardId" />
+            </div>
+            :
+          <>
             <div className="flex flex-row gap-8 mt-6">
-                <Bar percentage={scorePercentage} color="green" />
-                <Bar percentage={100 - scorePercentage} color="red" />
+              <Bar percentage={scorePercentage} color="green" />
+              <Bar percentage={100 - scorePercentage} color="red" />
             </div>
             <div className="flex flex-row gap-8">
-                <p>{score} Correct</p>
-                <p>{totalQuestions - score} Incorrect</p>
-            </div></>
-        </main>
+              <p>{score} Correct</p>
+              <p>{totalQuestions - score} Incorrect</p>
+            </div>
+          </>
+        }
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default QuizzSubmittion
+export default QuizzSubmittion;
